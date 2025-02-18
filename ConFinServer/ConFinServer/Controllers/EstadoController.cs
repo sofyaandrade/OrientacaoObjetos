4e4f37cef8f9f5a3eeecdef622cd6075990e4bf7
sofyaandrade <151881000+sofyaandrade@users.noreply.github.com>
2025-02-17 21:44:59 -0300

    Add files via upload

---
+++ ConFinServer/ConFinServer/Controllers/EstadoController.cs
@@ -0,0 +1,29 @@
+﻿using ConFinServer.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConFinServer.Controllers
+{
+    [Route("api/[controller]")] //anotation da rota, vai acessar o ip+porta/api/[nome do controller]
+    [ApiController]
+    public class EstadoController : ControllerBase //herança da rota
+    {
+        private static List<Estado> lista = new List<Estado>();
+        
+        [HttpGet]
+        public string GetEstado()
+        {
+            var valor = "teste";//f10 linha por linha e f9 pra disparar
+            valor = valor + "- BSN";
+            return valor;
+        }
+
+        [HttpGet("Get2")]
+        public string GetEstado2()
+        {
+            var valor = "teste";
+            valor = valor + "- BSN 2";
+            return valor;
+        }
+    }
+}
---
+++ ConFinServer/ConFinServer/Model/Estado.cs
@@ -0,0 +1,8 @@
+﻿namespace ConFinServer.Model
+{
+    public class Estado
+    {
+        public string Sigla { get; set; }
+        public string Nome { get; set; }
+    }
+}
