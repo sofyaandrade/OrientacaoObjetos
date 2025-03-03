1f320e8a5acbc65b37dd17a26c8731f124850bb2
sofyaandrade <151881000+sofyaandrade@users.noreply.github.com>
2025-03-03 20:47:00 -0300

    Add files via upload

---
+++ ConFinServer/ConFinServer/Model/Cidade.cs
@@ -0,0 +1,10 @@
+﻿namespace ConFinServer.Model
+{
+    public class Cidade
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Estado { get; set; }
+
+    }
+}
